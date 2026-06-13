e99fafb45ca8d92b6c423023cb05fa93e9877527
TungPT <tung.pham@ext.be.com.vn>
2026-06-13 08:59:01 +0700

    m

---
+++ frontend/build/precache-manifest.b4fe03a101d9069f71bdc97895a50ee9.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc61edffa2071ba8a15e3842d31797f7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aab104c71a55f065fb1c",
+    "url": "/static/css/2.dba70190.chunk.css"
+  },
+  {
+    "revision": "aab104c71a55f065fb1c",
+    "url": "/static/js/2.750f325c.chunk.js"
+  },
+  {
+    "revision": "ae4b388c3e5ebbf4878df518477e1ae5",
+    "url": "/static/js/2.750f325c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd6322d720a7eef9f508",
+    "url": "/static/js/main.66227000.chunk.js"
+  },
+  {
+    "revision": "ac59e94fdb04d1008c55",
+    "url": "/static/js/runtime-main.051f5436.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
